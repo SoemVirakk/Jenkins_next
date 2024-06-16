7a833531a747b51558cea7b5633249c29ce55a41
SoemVirakk <soemvirak90@gmail.com>
2024-06-16 13:34:48 +0700

    kak test

--- resources/api.dockerfile
+++ resources/api.dockerfile
@@ -13,7 +13,7 @@ FROM openjdk:17-alpine
 WORKDIR /app
 # Copy the built JAR from the builder stage and rename it for simplicity
 COPY --from=builder /app/build/libs/*0.0.1-SNAPSHOT.jar app.jar   #no need to change i already make it dynamic 
-EXPOSE 8089
+EXPOSE 8080
 VOLUME /home/ite/media
 VOLUME /keys   
 # Now you can reference a fixed name in the ENTRYPOINT
--- vars/apiWithDocker.groovy
+++ vars/apiWithDocker.groovy
@@ -2,4 +2,10 @@ def call() {
     def dockerfileContent = libraryResource 'api.dockerfile'
     writeFile file: 'Dockerfile', text: dockerfileContent
     // sh 'docker build -t myapp:latest .'
+    def image = cofig.get('image', 'my-default-image')
+    def registry =config.get('tag', 'latest') 
+    def tag = config.get('containerPort,''8080')
+    def hostPort = config.get('hostPort', '8080')
+    def projectName = config.get('projectName', 'my-projectname')
+    
 }
