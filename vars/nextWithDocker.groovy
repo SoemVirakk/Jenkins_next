76f6a880f127567e6fdfaf93add7623acb073800
SoemVirakk <soemvirak90@gmail.com>
2024-06-16 15:10:22 +0700

    test

--- vars/nextWithDocker.groovy
+++ vars/nextWithDocker.groovy
@@ -1,6 +1,7 @@
-def call(String DOCKER_USER, String DOCKER_PASS) {
+def call(String DOCKER_USER, String DOCKER_PASS, String TAGS, String IMAGE_NAME) {
     def dockerfileContent = libraryResource 'next.dockerfile'
     writeFile file: 'Dockerfile', text: dockerfileContent
     sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-    sh 'docker build -t myapp:latest .'
+    sh 'docker build -t myappspring:latest .'
+    sh 'docker push $DOCKER_USER/$MAGE_NAME:$TAGS'
 }
