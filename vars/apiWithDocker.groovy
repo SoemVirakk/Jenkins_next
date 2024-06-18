909bb037074ec8a37a9b7b5ae93f58fa99c8f802
SoemVirakk <soemvirak90@gmail.com>
2024-06-18 18:16:18 +0700

    Test

--- vars/apiWithDocker.groovy
+++ vars/apiWithDocker.groovy
@@ -1,7 +1,7 @@
 def call() {
-    def dockerfileContent = libraryResource 'api.dockerfile'
-    writeFile file: 'Dockerfile', text: dockerfileContent
-    // sh 'docker build -t myapp:latest .'
+    def dockerfileContent = libraryResource 'api.dockerfile' // call api dcoekr file
+    writeFile file: 'Dockerfile', text: dockerfileContent    // wim dcoeker file -t myapp:lest
+    echo('print docker fiels: 'dockerfilesContent)
     def image = cofig.get('image', 'my-default-image')
     def registry =config.get('tag', 'latest') 
     def tag = config.get('containerPort,''8080')
--- vars/nextWithDocker.groovy
+++ vars/nextWithDocker.groovy
@@ -1,7 +1,7 @@
 def call(String DOCKER_USER, String DOCKER_PASS, String TAGS, String IMAGE_NAME) {
     def dockerfileContent = libraryResource 'next.dockerfile'
     writeFile file: 'Dockerfile', text: dockerfileContent
-    sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-    sh 'docker build -t myappspring:latest .'
+    sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'  -- Docker_Tag
+    sh 'docker build -t myappspring:latest .'          -- docker build 
     sh 'docker push $DOCKER_USER/$MAGE_NAME:$TAGS'
 }
